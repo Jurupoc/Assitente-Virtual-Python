0056f35350fa6ae525c8d04914ca2864d97c4912
jurupocA <jurupoc222@gmail.com>
2021-08-11 00:19:17 -0300

    Adicionando Reconhecedor de Audio

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('Hello World')
+# Our Main File
+
+import speech_recognition as sr
+
+# Create the recognizer
+r = sr.Recognizer()
+
+# Open the audio device
+with sr.Microphone() as source:
+    while True:
+        audio = r.listen(source)
+        print(r.recognize_google(audio, language='pt'))
